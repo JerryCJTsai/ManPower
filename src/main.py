05b944c0364ee2a9b0afc17427f7d28c2c83ed56
VanWu1984 <define1984@gmail.com>
2024-11-13 23:44:43 +0800

    My First Window

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+from view.test import *
 
+class Demo(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 if __name__ == '__main__':
-    print('hello !!!')
+    app = QApplication(sys.argv)
+    win = Demo()
+    win.show()
+    sys.exit(app.exec_())
