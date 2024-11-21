8281c175dc26bcfa497966ff68b26b4473fcb96c
JerryCJTsai <yyaatim@gmail.com>
2024-11-21 21:50:43 +0800

    Say Name

--- app/app_main.py
+++ app/app_main.py
@@ -5,7 +5,9 @@ from PyQt5.QtWidgets import QApplication
 from view.manpower_view import MainView
 
 if __name__ == "__main__":
+    print("Jerry")
     app = QApplication(sys.argv)
     win = MainView()
     win.show()
     sys.exit(app.exec())
+
