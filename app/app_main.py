3a49fecf468e407bf33b199b2e592ab9838f2ceb
VanWu1984 <define1984@gmail.com>
2024-11-21 21:45:05 +0800

    Just Say Hello

--- app/app_main.py
+++ app/app_main.py
@@ -6,6 +6,7 @@ from view.manpower_view import MainView
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    print("Hello, World !!!")
     win = MainView()
     win.show()
     sys.exit(app.exec())
